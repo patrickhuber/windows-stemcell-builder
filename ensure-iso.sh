292836596e8bf8d0483847702f920d7d24bdbcb3
Patrick Huber <patrickhuber@hotmail.com>
2017-12-29 14:47:03 -0500

    updates to test environment variables

--- ensure-iso.sh
+++ ensure-iso.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # https://github.com/vmware/govmomi/blob/master/govc/USAGE.md#datastoreupload
-curl $iso | govc datastore.upload - $iso_folder/$iso_name
+# curl $iso | govc datastore.upload - $iso_folder/$iso_name
+echo $GOVC_URL
+echo $GOVC_PASSWORD
+echo $GOVC_USERNAME
+echo $GOVC_INSECURE
+echo $GOVC_DATACENTER
+echo $GOVC_DATASTORE
+echo $GOVC_NETWORK
+echo $GOVC_CLUSTER
+echo $iso
+echo $iso_name
+echo $iso_folder
